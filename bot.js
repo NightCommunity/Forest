32786980c3d34a0d579db71f7773b568f2a73c40
NightCommunity <42852008+NightCommunity@users.noreply.github.com>
2018-10-06 21:57:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -470,7 +470,7 @@ client.on('message', message => {
   ` + '`' + prefix + 'setStatus` - **لتغيير حالة البوت**' + `
 
 
-`Forèst To The Top`
+
   if(message.content === prefix + 'help') {
     message.author.send(helplist);
   }
